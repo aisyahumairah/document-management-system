9de315ea8d05cd1362208c5f03feb93e866958be
meyyy <87225444+aisyahumairah@users.noreply.github.com>
2023-07-02 22:39:07 +0800

    navbar updated

--- layout/navbar.php
+++ layout/navbar.php
@@ -58,22 +58,22 @@
                         <div class="dropdown-content-body">
                             <ul>
                                 <li>
-                                    <a href="app-profile.html"><i class="icon-user"></i>
+                                    <a href="../profile.php"><i class="icon-user"></i>
                                         <span>Profile</span></a>
                                 </li>
-                                <li>
+                                <!-- <li>
                                     <a href="javascript:void()">
                                         <i class="icon-envelope-open"></i> <span>Inbox</span>
                                         <div class="badge gradient-3 badge-pill gradient-1">3</div>
                                     </a>
-                                </li>
+                                </li> -->
 
                                 <hr class="my-2">
-                                <li>
+                                <!-- <li>
                                     <a href="page-lock.html"><i class="icon-lock"></i> <span>Lock
                                             Screen</span></a>
-                                </li>
-                                <li><a href="page-login.html"><i class="icon-key"></i> <span>Logout</span></a>
+                                </li> -->
+                                <li><a href=""><i class="icon-key"></i> <span>Logout</span></a>
                                 </li>
                             </ul>
                         </div>
