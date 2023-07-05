a40dc46d63e53be1680cc4af1548ce852a37d909
meyyy <87225444+aisyahumairah@users.noreply.github.com>
2023-07-05 22:34:15 +0800

    Update connection.php

--- connection.php
+++ connection.php
@@ -9,8 +9,8 @@ $mysqli = new mysqli($servername, $username, $password, $dbname);
 //check connection
 if($mysqli)
 {
-    die("Connection failed: " . mysqli_connect_error());
+    echo "Connected Successfully";
 }
-echo "Connected Successfully";
+die("Connection failed: " . mysqli_connect_error());
 
 ?>
