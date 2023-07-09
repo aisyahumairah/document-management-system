636a96562dbd683f6edc369ba8d33909dc29a7ef
meyyy <87225444+aisyahumairah@users.noreply.github.com>
2023-07-09 22:36:06 +0800

    Update sidebar.php

--- layout/sidebar.php
+++ layout/sidebar.php
@@ -2,27 +2,13 @@
 <div class="nk-sidebar">
     <div class="nk-nav-scroll">
         <ul class="metismenu" id="menu">
-            <!-- <li class="nav-label">Dashboard</li> -->
-
 
             <li>
                 <a href="../folder-dashboard/dashboard.php" aria-expanded="false">
                     <i class="icon-speedometer menu-icon"></i><span class="nav-text">Dashboard</span>
                 </a>
-                <!-- <ul aria-expanded="false">
-                    <li><a href="../folder-user/indexUser.php">Pengguna</a></li>
-                    <li><a href="./index-2.html">Home 2</a></li>
-                </ul> -->
             </li>
 
-            <!-- <li class="mega-menu mega-menu-sm">
-                <a class="has-arrow" href="javascript:void()" aria-expanded="false">
-                    <i class="icon-globe-alt menu-icon"></i><span class="nav-text">Dokumen</span>
-                </a>
-                <ul aria-expanded="false">
-                    <li><a href="../folder-doc/indexDoc.php">Dokumen Terkawal</a></li>
-                </ul>
-            </li> -->
 
             <li>
                 <a href="../folder-doc/indexDoc.php" aria-expanded="false">
